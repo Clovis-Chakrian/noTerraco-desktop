f798a203b4edff999d0f306111ad5bae1f6e741b
Clóvis Chakrian <clovischakriandev@gmail.com>
2022-07-25 11:25:04 -0300

    this file point to the div with id root at index.html to render react code

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './index_react'
+
+function render() {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+render();
